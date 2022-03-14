1bbcf7b7a2184ee6277975a81b3dbc81ab2ee7fd
BrendenSeifried <brendenseifried@gmail.com>
2022-03-14 12:30:37 -0700

    added blogs.js to views

--- src/views/Main.js
+++ src/views/Main.js
@@ -3,7 +3,13 @@ import React from 'react';
 import BlogCard from '../BlogCard/BlogCard.js';
 
 export default function main() {
-  const [ title, subtitle, text ,image ]
+  const [title, subtitle, text, image] = useState([]);
+
+  useEffect(()=>{
+    const fetchData = async () => {
+      const resp = await fetchBlog;
+    };
+  }); 
   return (
     
     <div>main</div>

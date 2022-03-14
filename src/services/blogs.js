f784ac14c003f737e30905bc32da6c06c0428365
BrendenSeifried <brendenseifried@gmail.com>
2022-03-14 12:43:45 -0700

    denver helped me set up blog.js i was overthinking it

---
+++ src/services/blogs.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import { client, checkError } from './client';
+
+export default function getBlog() {
+    const response = await client.from('blogs').select('*');
+    return checkError(response);
+}

0b1760cb8b8917e90cc52647db4e2186a6f5f20f
BrendenSeifried <brendenseifried@gmail.com>
2022-03-14 12:23:00 -0700

    added Main to app.js

--- src/views/Main.js
+++ src/views/Main.js
@@ -1,7 +1,11 @@
 import React from 'react';
 
+import BlogCard from '../BlogCard/BlogCard.js';
+
 export default function main() {
+  const [ title, subtitle, text ,image ]
   return (
+    
     <div>main</div>
   );
 }

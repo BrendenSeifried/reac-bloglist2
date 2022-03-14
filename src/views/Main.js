abf158780bab6fdb0640e42e43c4412fae94f8a1
BrendenSeifried <brendenseifried@gmail.com>
2022-03-14 12:46:57 -0700

    got blog imports into main

--- src/views/Main.js
+++ src/views/Main.js
@@ -2,6 +2,10 @@ import React from 'react';
 
 import BlogCard from '../BlogCard/BlogCard.js';
 
+import { useState, useEffect } from 'react/cjs/react.production.min';
+
+import { getBlog } from '../services/blogs.js';
+
 export default function main() {
   const [title, subtitle, text, image] = useState([]);
 
